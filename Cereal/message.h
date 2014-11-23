00fe0101945a68956004964081b330124ba0efd7
Dima <dmitry.korolev@gmail.com>
2014-11-23 12:43:06 -0800

    Cereal demo.

---
+++ Cereal/message.h
@@ -0,0 +1,76 @@
+#ifndef SANDBOX_CEREAL_MESSAGE_H
+#define SANDBOX_CEREAL_MESSAGE_H
+
+#include "cereal/archives/binary.hpp"
+#include "cereal/archives/portable_binary.hpp"
+#include "cereal/archives/json.hpp"
+
+#include "cereal/types/string.hpp"
+
+// TODO(dkorolev): Add these types, as well as many others. Although I have tested those myself.
+
+// #include "cereal/types/vector.hpp"
+// #include "cereal/types/map.hpp"
+
+#include "cereal/types/polymorphic.hpp"
+
+// Objects of derived types should be wrapped into a smart pointer to an instance of their base class
+// for cereal to serialize them correcly with the type information encoded.
+//
+// Use this construct:
+//
+//   DerivedObject object;
+//   cereal_archive(SerializeAsPolymorphic<Base>(object));
+
+template <typename T>
+struct PolymorphicSerializerNullDeleter {
+  inline void operator()(T*) {
+  }
+};
+
+template <typename T>
+std::unique_ptr<T, PolymorphicSerializerNullDeleter<T>> SerializeAsPolymorphic(T& object) {
+  return std::unique_ptr<T, PolymorphicSerializerNullDeleter<T>>(&object);
+}
+
+enum class NonStandardInt : int;
+
+struct BaseClass {
+  virtual std::string AsString() const = 0;
+};
+
+struct DerivedClassInt : BaseClass {
+  NonStandardInt x;
+  virtual std::string AsString() const {
+    std::ostringstream os;
+    os << "Int: " << int(x);
+    return os.str();
+  }
+
+ private:
+  friend class cereal::access;
+  template <class A>
+  void serialize(A& ar) {
+    ar(CEREAL_NVP(x));
+  }
+};
+CEREAL_REGISTER_TYPE(DerivedClassInt);
+
+struct DerivedClassString : BaseClass {
+  std::string s;
+  virtual std::string AsString() const {
+    std::ostringstream os;
+    os << "String: " << s;
+    return os.str();
+  }
+
+ private:
+  friend class cereal::access;
+  template <class A>
+  void serialize(A& ar) {
+    ar(CEREAL_NVP(s));
+  }
+};
+CEREAL_REGISTER_TYPE(DerivedClassString);
+
+#endif  // SANDBOX_CEREAL_MESSAGE_H
